42558fabb091ec6c4814f2737e83e10e89175bbf
NazarKravchuk <Nazar.kravchuk@ukr.net>
2017-01-13 12:08:51 +0200

    Add project files.

---
+++ InvokeCount/ManagerInvoke.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InvokeCount
+{
+    public class ManagerInvoke
+    {
+        public int TimeInvoke { get; }
+        public int Count { get; private set; }
+        public Action Method { get; }
+
+        private Thread task;
+
+        public ManagerInvoke(Action action, int timeInvoke)
+        {
+            TimeInvoke = timeInvoke;
+            Method = action;
+        }
+        public void Invoke()
+        {
+            Method.Invoke();
+            Count++;
+        }
+        public void Start()
+        {
+            task?.Abort(); 
+            task = new Thread(InvokeCount);
+            task.IsBackground = true;
+            task.Start();
+        }
+
+        public void Stop()
+        {
+            task?.Abort();
+        }
+        public void InvokeCount()
+        {
+            while (true)
+            {
+                Thread.Sleep(TimeInvoke);
+                OnCountInvoke(Count);
+                Count = 0;
+            }
+        }
+
+        public event EventHandler<int> CountInvoke;
+
+        protected virtual void OnCountInvoke(int e)
+        {
+            CountInvoke?.Invoke(this, e);
+        }
+    }
+}
---
+++ InvokeCount/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InvokeCount
+{
+    class Program
+    {
+        public static ManagerInvoke managerInvoke;
+        static void Main(string[] args)
+        {
+            managerInvoke = new ManagerInvoke(() =>
+            {
+                Thread.Sleep(800);
+                Console.WriteLine("!");
+            }, 1000);
+            managerInvoke.CountInvoke += ManagerInvokeCountInvoke;
+            managerInvoke.Start();
+            var task = new Timer(Dub);
+            task.Change(25, 10);
+            Console.ReadKey();
+            managerInvoke.Stop();
+            task.Dispose();
+            //   task.InitializeLifetimeService();
+        }
+
+        private static void Dub(object state)
+        {
+            managerInvoke.Invoke();
+        }
+
+        private static void ManagerInvokeCountInvoke(object sender, int e)
+        {
+            Console.WriteLine($"Count invoke = {e}");
+        }
+    }
+}
